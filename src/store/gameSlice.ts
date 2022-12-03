245b6fd6ea993f990df79367722f598c1beb9cd2
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2022-12-03 14:34:47 -0500

    finished logic to decide when the game is over

--- src/store/gameSlice.ts
+++ src/store/gameSlice.ts
@@ -4,6 +4,8 @@ import { CellStatusChangePayload, CellType } from "../types";
 interface GameState {
   cellsState: CellType[][];
   actualTurn: CellType;
+  winningCells?: number[];
+  winnerTurn: CellType;
 }
 
 const initialState: GameState = {
@@ -13,6 +15,8 @@ const initialState: GameState = {
     [undefined, undefined, undefined],
   ],
   actualTurn: "noughts",
+  winningCells: [0, 0, 0],
+  winnerTurn: undefined,
 };
 
 export const gameSlice = createSlice({
@@ -25,12 +29,21 @@ export const gameSlice = createSlice({
     ) => {
       const { payload } = action;
       const { row, column } = payload;
+      if (state.cellsState[row][column] !== undefined) {
+        return;
+      }
       state.cellsState[row][column] = state.actualTurn;
       state.actualTurn = state.actualTurn === "noughts" ? "crosses" : "noughts";
+    },
+    setWinningCells: (state, action: PayloadAction<number[]>) => {
+      state.winningCells = action.payload;
+      state.winnerTurn = state.actualTurn === "noughts" ? "crosses" : "noughts";
+      console.log("winningCells: ", state.winningCells);
+      console.log("winnerTurn: ", state.winnerTurn);
     },
   },
 });
 
-export const { changeCellStatus } = gameSlice.actions;
+export const { changeCellStatus, setWinningCells } = gameSlice.actions;
 
 export default gameSlice.reducer;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,43 @@
 import { configureStore } from "@reduxjs/toolkit";
-import gameReducer from "./gameSlice";
+import { CellType } from "../types";
+import gameReducer, { setWinningCells } from "./gameSlice";
 
 export const store = configureStore({
   reducer: {
     game: gameReducer,
   },
+});
+
+store.subscribe(() => {
+  const { game } = store.getState();
+  const { winnerTurn } = game;
+  if (winnerTurn) return;
+  const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  const cellsState = game.cellsState;
+  const valuesFromCells: CellType[][] = [];
+  winningConditions.forEach((combination) => {
+    const temp = combination.map((cellNumber) => {
+      const row = Math.floor(cellNumber / 3);
+      const column = cellNumber % 3;
+      return cellsState[row][column];
+    });
+    valuesFromCells.push(temp);
+  });
+  valuesFromCells.forEach((cells: CellType[], index: number) => {
+    if (cells[0] && cells.every((cell) => cell === cells[0])) {
+      store.dispatch(setWinningCells(winningConditions[index]));
+      return;
+    }
+  });
 });
 
 export type RootState = ReturnType<typeof store.getState>;
